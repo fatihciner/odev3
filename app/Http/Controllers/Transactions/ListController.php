c1706a076a9c952c01e9f9868332fd0c32df506c
vagrant <vagrant@homestead>
2017-08-12 00:46:28 +0300

    bootstrap ok

--- app/Http/Controllers/Transactions/ListController.php
+++ app/Http/Controllers/Transactions/ListController.php
@@ -2,35 +2,16 @@
 /**
  * Created by PhpStorm.
  * User: fyilmaz
- * Date: 9.08.2017
- * Time: 18:52
+ * Date: 12.08.2017
+ * Time: 00:41
  */
-
 namespace app\Http\Controllers\Transactions;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\EFT\DataFetcher;
-
 class ListController extends Controller
 {
-
-	protected $dataTransporter;
-
-	/**
-	 * ListController constructor.
-	 */
-	public function __construct(DataFetcher $dataFetcher)
-	{
-		$this->dataTransporter = $dataFetcher;
-	}
-
-
-
 	public function index()
 	{
-
-		$output = $this->dataTransporter->getResponse('http://www.bloomberght.com');
-		dd($output);
-		return view('transaction.list');
+		return view('pages.transaction.list');
 	}
 }
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,4 +7,3 @@
 <!-- load bootstrap from a cdn -->
 <!-- <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css"> -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-
